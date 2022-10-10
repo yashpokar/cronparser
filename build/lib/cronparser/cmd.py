5001c5183034c77cea0e3abd07784b5ffb763db4
Yash Pokar <hello@yashpokar.com>
2022-10-10 09:43:32 +0530

    added build in .gitignore

--- build/lib/cronparser/cmd.py
+++
@@ -1,8 +0,0 @@
-"""Cron Parser command line utility to run"""
-import sys
-from .parser import parse
-
-def main():
-    """main will get invoked when command line interface will be used"""
-    result = parse(sys.argv[1:])
-    print(result)
